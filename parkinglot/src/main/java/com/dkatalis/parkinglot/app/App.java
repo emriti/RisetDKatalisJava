e8a6bbe31bd0e75e052544f90da56307140405b1
Ahmad Fajri <fajri@ecomindo.onmicrosoft.com>
2020-05-04 16:40:42 +0700

    initialized

---
+++ parkinglot/src/main/java/com/dkatalis/parkinglot/app/App.java
@@ -0,0 +1,13 @@
+package com.dkatalis.parkinglot.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
