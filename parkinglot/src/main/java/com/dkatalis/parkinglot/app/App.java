7840aafcb457199cab0ae29ebb12ca1eb07ec115
Ahmad Fajri <fajri@ecomindo.onmicrosoft.com>
2020-05-05 17:22:46 +0700

    setup maven for running the app in the console

--- parkinglot/src/main/java/com/dkatalis/parkinglot/app/App.java
+++ parkinglot/src/main/java/com/dkatalis/parkinglot/app/App.java
@@ -1,11 +1,54 @@
 package com.dkatalis.parkinglot.app;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.dkatalis.parkinglot.service.ParkingService;
+
 /**
  * Hello world!
  *
  */
 public class App {
+
+	private static ParkingService svc;
+
 	public static void main(String[] args) {
-		
+
+		svc = new ParkingService();
+
+		if (args.length > 0) {
+			// get command
+			String cmd1 = args[0];
+
+			// run the app with file
+			if (cmd1.split(".txt").length > 0) {
+				try (BufferedReader br = new BufferedReader(new FileReader(cmd1))) {
+					String line;
+					while ((line = br.readLine()) != null) {
+						RunningApp(line);
+					}				
+				}
+				catch (FileNotFoundException e) {
+					System.out.println("File not found!");
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		} else {
+			// run interactively
+			while (true) {
+				String input = System.console().readLine();
+				RunningApp(input);
+			}
+		}
+
 	}
+
+	private static void RunningApp(String input) {
+		System.out.println(input);
+	}
+
 }
